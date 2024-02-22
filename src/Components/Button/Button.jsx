c76d24d203371cf5766dec81d11a4bde21b79e28
pavanrteli <pavanrteli@gmail.com>
2024-02-22 21:42:39 +0530

    NavBar

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -4,7 +4,7 @@ import styles from "./Button.module.css";
 export default function Button(){
     return (
         <button className={styles.Button}>
-            Give FeedBack
+            Give Feedback
         </button>
     )
 }
