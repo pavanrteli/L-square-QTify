d1e66794503e438eb72846d2995ed5c871eec670
pavanrteli <pavanrteli@gmail.com>
2024-02-23 17:53:04 +0530

    Hero image

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div>
       <NavBar/>
-      {/* <Hero/> */}
+      <Hero/>
     </div>
   );
 }
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,12 +6,12 @@ function Hero() {
 
   return (
     <div className={styles.Hero}>
-        <div>
+        <div className={styles.fontColor}>
             <h2>100 Thousands Songs, add-free</h2>
             <h2>Over thousands podcast episodes</h2>
         </div>
         <div>
-            <img src={HeroImage} alt="HeroImage" />
+            <img src={HeroImage} alt="HeroImage" width="212px" />
         </div>
     </div>
   )
